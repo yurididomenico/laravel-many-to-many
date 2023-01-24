66227ca779e8c0de2e1bfdae16769242dbc8f65b
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-01-24 17:36:56 +0100

    Inserisco Vue :)

---
+++ resources/js/views/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Work in Progress...</h1>
+    </div>
+</template>
+
+<script>
+export default
+{
+    name: "App",
+    components:
+    {
+        //Futuri componenti per questa view
+    }
+}
+</script>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,5 +1,16 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Front</title>
+</head>
+<body>
+    {{-- Inseriamo Vue --}}
+    <div id="root">
 
-@section('content')
-
-@endsection
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
