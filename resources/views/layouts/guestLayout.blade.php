034847eef78eb2522e1564a4f741604d58057384
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-01-25 13:46:14 +0100

    Fix struttura cartelle e nomi files

--- resources/views/layouts/guestLayout.blade.php
+++ resources/views/layouts/guestLayout.blade.php
@@ -1,22 +1,14 @@
-@extends('layouts.guestLayout')
-
-@section('content')
-
-@endsection
-{{-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Front</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Guest Vue</title>
 </head>
 <body>
+    @yield('content')
 
-    <div id="root">
-
-    </div>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
-</html> --}}
+</html>
