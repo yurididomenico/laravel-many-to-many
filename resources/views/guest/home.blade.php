46f044a68c8af729e2ef0268b858a06b5d696e66
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-01-25 13:47:11 +0100

    Vue in guestLayout

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -2,21 +2,9 @@
 
 @section('content')
 
-@endsection
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Front</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-</head>
-<body>
-
     <div id="root">
 
     </div>
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html> --}}
+
+@endsection
+
