7fa4a58562b524918143cf3ab8b02f4261928e8b
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-01-25 14:43:09 +0100

    aggiunto use App\Post

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -15,11 +16,12 @@ class PostController extends Controller
      */
     public function index()
     {
-        $userId = Auth::id();
-        $user = Auth::user();
+        // $userId = Auth::id();
+        // $user = Auth::user();
 
+        $posts = Post::paginate(10);
 
-        return view('admin.post.index', compact('userId', 'user'));
+        return view('admin.post.index', compact('posts'));
     }
 
     /**
