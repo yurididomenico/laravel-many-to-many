4b984bc8f9aedcabd48a6cb89ff13903bdee4f31
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-01-26 15:16:31 +0100

    DatabaseSeeder fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PostSeeder::class);
+        $this->call(
+            [
+                PostSeeder::class,
+                CategorySeeder::class
+            ]
+        );
     }
 }
